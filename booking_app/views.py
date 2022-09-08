09601a367e2e95abad014c246b6c698906a6e315
hashim222 <aslamhashim19@gmail.com>
2022-09-08 11:47:32 +0000

    created base html file and connected external files

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class test_if_templates_works(TemplateView):
+    template_name = 'base.html'
+
