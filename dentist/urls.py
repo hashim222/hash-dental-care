26a83dd48dff1a6d1d783e00a6e67869820f4f98
hashim222 <aslamhashim19@gmail.com>
2022-09-22 16:17:16 +0000

    installed django-allauth library and in the html templates few buttons tidy up

--- dentist/urls.py
+++ dentist/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking_app.urls')),
     path('', include('treatments.urls')),
-    path('', include('contact.urls'))
+    path('', include('contact.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
