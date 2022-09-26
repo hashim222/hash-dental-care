ab78d03dbc61287a55fc2810b32a8fcb1d300269
hashim222 <aslamhashim19@gmail.com>
2022-09-26 00:14:49 +0000

    updated footer styling for my project

--- dentist/settings.py
+++ dentist/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'hash-dental-care.herokuapp.com', '127.0.0.1', 'localhost'
